0ead06b3f2b93facfe65a3e69b73a2896ddba685
Maurino Federico Hernandez <maurinofederico96@gmail.com>
2020-12-30 19:11:53 -0500

    Created current day display

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,10 @@
-var currentDate = moment().format("dddd MMMM Do")
+
+var currentDayEl = $("#currentDay");
+var containerTime = $(".container");
+
+//Displays Current date on currentDay p tag//
+var currentDay = (moment().format("dddd MMMM Do"));
+currentDayEl.html(currentDay);
+
+
+
