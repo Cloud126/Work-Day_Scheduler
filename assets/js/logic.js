7e2ff0a29c120e0890c00b62195cc1b189bb6e97
Maurino Federico Hernandez <maurinofederico96@gmail.com>
2020-12-30 22:30:56 -0500

    Created time blocks dynamically in JS file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,42 @@
-
 var currentDayEl = $("#currentDay");
-var containerTime = $(".container");
+var containerEl = $(".container");
+var currentDay = (moment().format("dddd MMMM Do"));
 
 //Displays Current date on currentDay p tag//
-var currentDay = (moment().format("dddd MMMM Do"));
 currentDayEl.html(currentDay);
 
+//Array for times//
+var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+
+
+//Creates time blocks//
+for (var i = 0; i < times.length; i++) {
+
+    var row = $("<div>")
+    var timeBlock = $("<div>")
+    var textArea = $("<textarea>")
+    var spanTime = $("<span>")
+    var btnDiv = $("<div>")
+    var saveBtn = $("<button>")
+
+    spanTime.text(times[i])
+
+    row.addClass("input-group mb-3");
+    timeBlock.addClass("input-group-prepend");
+    spanTime.addClass("input-group-text time-block")
+    textArea.addClass("form-control");
+    saveBtn.addClass("saveBtn fas fa-save")
+    btnDiv.addClass("input-group-append")
+    
+    containerEl.append(row);
+    row.append(timeBlock);
+    row.append(textArea);
+    row.append(btnDiv);
+    
+    btnDiv.append(saveBtn);
+    timeBlock.append(spanTime);
+    
+};
 
 
