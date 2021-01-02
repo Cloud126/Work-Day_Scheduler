3569135673effa72d7d87162022c0cc7b7f849c1
Maurino Federico Hernandez <maurinofederico96@gmail.com>
2021-01-02 16:38:06 -0500

    Created click function to target textarea

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,20 +10,21 @@ var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 //military time//
 var milTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-for (var i = 0; i < times.length; i++) {
+//For loop to loop through milTime and times
+for (var i = 0; i < 9; i++) {
 
     //dynamic html elements//
-    var row = $("<div>")
-    var timeBlock = $("<div>")
-    var textArea = $("<textarea>")
-    var saveBtn = $("<button>")
+    var row = $("<div>");
+    var timeBlock = $("<div>");
+    var textArea = $("<textarea>");
+    var saveBtn = $("<button>");
     
     //adds value and id to textarea div
-    textArea.attr('value', milTime[i])
-    var textId = textArea.attr('id', milTime[i])
+    textArea.attr('value', milTime[i]);
+    var textId = textArea.attr('id', milTime[i]);
     
     //displays hour for each row//
-    timeBlock.text(times[i])
+    timeBlock.text(times[i]);
 
     //adds data value to each button
     saveBtn.attr("value", milTime[i]);
@@ -32,7 +33,7 @@ for (var i = 0; i < times.length; i++) {
     row.addClass("rowDiv input-group mb-0.1 row");
     timeBlock.addClass("timeBlock input-group-prepend col-1 hour");
     textArea.addClass("textArea description col-10");
-    saveBtn.addClass("saveBtn fas fa-save col-1")
+    saveBtn.addClass("saveBtn fas fa-save col-1");
 
     //appends html elements//
     containerEl.append(row);
@@ -40,15 +41,11 @@ for (var i = 0; i < times.length; i++) {
     row.append(textArea);
     row.append(saveBtn);
 
-    //saveBtn that targets each button value
-    saveBtn.click(function (e) {
-    console.log($(this).prev().val())
-    });
-
-    var currentHour = new Date().getHours()
+    //Gets current hour of the day
+    var currentHour = new Date().getHours();
 
     //Converts milTime array to interger
-    var hourRow = parseInt(milTime[i])
+    var hourRow = parseInt(milTime[i]);
 
     //Color codes textarea depending on current time
     if(hourRow === currentHour){
@@ -58,7 +55,15 @@ for (var i = 0; i < times.length; i++) {
     } else if(hourRow < currentHour) {
         textId.addClass("past")
     }
-    
+
+    //saveBtn that targets each button value
+    saveBtn.click(function (e) {
+    var text = JSON.stringify($(this).prev().val())
+    localStorage.setItem("Content", text)
+    });
+   
+    var savedText = JSON.parse(localStorage.getItem("text"))
+    console.log(savedText)
 };
 
     
